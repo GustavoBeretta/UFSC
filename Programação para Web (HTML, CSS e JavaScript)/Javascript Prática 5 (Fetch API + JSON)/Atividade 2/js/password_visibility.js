84347d947c4381f59fa0e53f90db3d7aee68fb2d
Gustavo Beretta Gonçalves <gustavoberettag@gmail.com>
2024-06-06 17:17:36 -0300

    prática 5 atividade 2 quase finalizada

---
+++ Programação para Web (HTML, CSS e JavaScript)/Javascript Prática 5 (Fetch API + JSON)/Atividade 2/js/password_visibility.js
@@ -0,0 +1,14 @@
+function visibilidadeSenha() {
+    if (document.getElementById('login-password').type == 'password') {
+        document.getElementById('login-password').type = 'text'
+        document.getElementById('olho').src = "https://cdn4.iconfinder.com/data/icons/symbol-blue-set-1/100/Untitled-2-34-512.png"
+    } else {
+        document.getElementById('login-password').type = 'password'
+        document.getElementById('olho').src = "https://cdn0.iconfinder.com/data/icons/ui-icons-pack/100/ui-icon-pack-14-512.png"
+    }
+}
+
+function ocultaSenha() {
+    document.getElementById('login-password').type = 'password'
+    document.getElementById('olho').src = "https://cdn0.iconfinder.com/data/icons/ui-icons-pack/100/ui-icon-pack-14-512.png"
+}
---
+++ Programação para Web (HTML, CSS e JavaScript)/Javascript Prática 5 (Fetch API + JSON)/Atividade 2/js/script.js
@@ -0,0 +1,250 @@
+function mostrarApenasHome() {
+    document.getElementById('divHome').style.display = 'block'
+    document.getElementById('login-body').style.display = 'none'
+    document.getElementById('nova-conta').style.display = 'none'
+}
+
+function mostrarApenasLogin() {
+    document.getElementById('divHome').style.display = 'none'
+    document.getElementById('login-body').style.display = 'block'
+    document.getElementById('nova-conta').style.display = 'none'
+    document.getElementById('campo-email').value=''
+    document.getElementById('login-password').value=''
+
+}
+
+function mostrarApenasConta() {
+    document.getElementById('divHome').style.display = 'none'
+    document.getElementById('login-body').style.display = 'none'
+    document.getElementById('nova-conta').style.display = 'block'
+    document.getElementById('nome').value=''
+    document.getElementById('sobrenome').value=''
+    document.getElementById('cpf').value=''
+    document.getElementById('email').value=''
+    document.getElementById('senha').value=''
+    document.getElementById('senhaa').value=''
+    document.getElementById('cep').value=''
+    document.getElementById('logradouro').value=''
+    document.getElementById('bairro').value=''
+    document.getElementById('cidade').value=''
+    document.getElementById('estado').value=''
+    document.getElementById('statusNome').style.display = 'none'
+    document.getElementById('statusSobrenome').style.display = 'none'
+    document.getElementById('statusCPF').style.display = 'none'
+    document.getElementById('statusEmail').style.display = 'none'
+    document.getElementById('statusSenha').style.display = 'none'
+    document.getElementById('statusRepitaSenha').style.display = 'none'
+    document.getElementById('statuscep').style.display = 'none'
+    document.getElementById('statuslogradouro').style.display = 'none'
+    document.getElementById('statusbairro').style.display = 'none'
+    document.getElementById('statuscidade').style.display = 'none'
+    document.getElementById('statusestado').style.display = 'none'
+    document.getElementById('BotaoConta').disabled = true
+}
+
+function camposPreenchidos() {
+    if (document.getElementById('login-password').value && document.getElementById('campo-email').value.split('@').length == 2) {
+        document.getElementById('botaoLogin').disabled = false
+    } else {
+        document.getElementById('botaoLogin').disabled = true
+    }
+}
+
+function checaTexto(id, status) {
+    if (document.getElementById(id).value) {
+        document.getElementById(status).innerText = '✅'
+        return true
+    } else {
+        document.getElementById(status).innerText = 'Não validado: o campo deve conter pelo menos um caracter'
+        return false
+    }
+}
+
+class CPF{
+    constructor(cpf){
+        this.cpf = cpf
+        if (!this.validarCPF()) {
+            throw new Error('Não validado')
+        }
+    }
+
+    haOnzeDigitos(cpf) {
+        let n = cpf.length
+        return n == 11
+    }
+    
+    todosOsOnzeDigitosSaoNumeros(cpf) {
+        let list = cpf.split("")
+        for (let i = 0; i < cpf.length; i++) {
+            if (isNaN(list[i])) {
+                return false
+            }
+        }
+        return true
+    }
+    
+    osOnzeNumerosSaoDiferentes(cpf) {
+        let list = cpf.split("")
+        for (let i = 1; i < cpf.length; i++) {
+            if (list[i] != list[i-1]){
+                return true
+            }
+        }
+        return false
+    }
+    
+    oPrimeiroDigitoVerificadorEhValido(cpf) {
+        let list = cpf.split("")
+        let soma = 0
+        let n = 10
+        for (let i = 0; i < 9; i++) {
+            soma += Number(list[i])*n
+            n -= 1
+        }
+        let resto = (soma * 10) % 11
+        if (resto == 10) {
+            resto = 0
+        }
+        if (resto == list[9]) {
+            return true
+        }
+        return false
+    }
+    
+    oSegundoDigitoVerificadorEhValido(cpf) {
+        let list = cpf.split("")
+        let soma = 0
+        let n = 11
+        for (let i = 0; i < 10; i++) {
+            soma += Number(list[i])*n
+            n -= 1
+        }
+        let resto = (soma * 10) % 11
+        if (resto == 10) {
+            resto = 0
+        }
+        if (resto == list[10]) {
+            return true
+        }
+        return false
+    }
+
+    validarCPF() {
+        return (this.haOnzeDigitos(this.cpf) && this.todosOsOnzeDigitosSaoNumeros(this.cpf) && this.osOnzeNumerosSaoDiferentes(this.cpf) && this.oPrimeiroDigitoVerificadorEhValido(this.cpf) && this.oSegundoDigitoVerificadorEhValido(this.cpf))
+    }
+}
+
+function checaCPF(){
+    try {
+        new CPF(document.getElementById('cpf').value)
+        document.getElementById('statusCPF').innerText = '✅'
+        return true
+    } catch(erro) {
+        document.getElementById('statusCPF').innerText = erro.message
+        return false
+    }
+}
+
+function checaSenha() {
+    if (document.getElementById('senhaa').value && document.getElementById('senhaa').value == document.getElementById('senha').value) {
+        document.getElementById('statusRepitaSenha').innerText = '✅'
+        return true
+    } else {
+        document.getElementById('statusRepitaSenha').innerText = 'Não validado: a senha deve conter pelo menos um caracter e ser igual a digitada anteriormente'
+        return false
+    }
+}
+
+function checaEmail() {
+    if (document.getElementById('email').value.split('@').length == 2) {
+        document.getElementById('statusEmail').innerText = '✅'
+        return true
+    } else {
+        document.getElementById('statusEmail').innerText = 'Não validado: o email deve conter um e apenas um "@"'
+        return false
+    }
+}
+
+async function checaCEP() {
+    if (document.getElementById('cep').value.length == 8) {
+        const response = await fetch(`https://viacep.com.br/ws/${document.getElementById('cep').value}/json/`)
+        const endereco = await response.json()
+        if (!endereco.erro) {
+            document.getElementById('statuscep').innerText = '✅'
+            document.getElementById('logradouro').value = endereco.logradouro
+            document.getElementById('bairro').value = endereco.bairro
+            document.getElementById('cidade').value = endereco.localidade
+            document.getElementById('estado').value = endereco.uf
+            validaCriacao('statuslogradouro')
+            validaCriacao('statusbairro')
+            validaCriacao('statuscidade')
+            validaCriacao('statusestado')
+            return true
+        }
+    }
+    document.getElementById('statuscep').innerText = 'Não validado'
+    return false
+}
+
+function validaCriacao(status_atual) {
+    let nome = checaTexto("nome", "statusNome")
+    let sobrenome = checaTexto("sobrenome", "statusSobrenome")
+    let cpf = checaCPF()
+    let email = checaEmail()
+    let senha = checaTexto("senha", "statusSenha")
+    let senhaa = checaSenha()
+    let cep = checaCEP()
+    let logradouro = checaTexto("logradouro", "statuslogradouro")
+    let bairro = checaTexto("bairro", "statusbairro")
+    let cidade = checaTexto("cidade", "statuscidade")
+    let estado = checaTexto("estado", "statusestado")
+    if (nome && sobrenome && cpf && email && senha && senhaa && cep && logradouro && bairro && cidade && estado) {
+        document.getElementById('BotaoConta').disabled = false
+    } else {
+        document.getElementById('BotaoConta').disabled = true
+    }
+    document.getElementById(status_atual).style.display = 'block'
+}
+
+function criaConta() {
+    let conta = new Conta(document.getElementById('nome').value, document.getElementById('sobrenome').value, new CPF(document.getElementById('cpf').value), document.getElementById('email').value, document.getElementById('senha').value)
+    console.log(conta)
+    document.getElementById('nome').value=''
+    document.getElementById('sobrenome').value=''
+    document.getElementById('cpf').value=''
+    document.getElementById('email').value=''
+    document.getElementById('senha').value=''
+    document.getElementById('senhaa').value=''
+    document.getElementById('cep').value=''
+    document.getElementById('logradouro').value=''
+    document.getElementById('bairro').value=''
+    document.getElementById('cidade').value=''
+    document.getElementById('estado').value=''
+    document.getElementById('statusNome').style.display = 'none'
+    document.getElementById('statusSobrenome').style.display = 'none'
+    document.getElementById('statusCPF').style.display = 'none'
+    document.getElementById('statusEmail').style.display = 'none'
+    document.getElementById('statusSenha').style.display = 'none'
+    document.getElementById('statusRepitaSenha').style.display = 'none'
+    document.getElementById('statuscep').style.display = 'none'
+    document.getElementById('statuslogradouro').style.display = 'none'
+    document.getElementById('statusbairro').style.display = 'none'
+    document.getElementById('statuscidade').style.display = 'none'
+    document.getElementById('statusestado').style.display = 'none'
+    document.getElementById('BotaoConta').disabled = true
+}
+
+class Conta{
+    constructor(nome, sobrenome, cpf, email, senha, cep, logradouro, bairro, cidade, estado) {
+        this.nome = nome
+        this.sobrenome = sobrenome
+        this.cpf = cpf
+        this.email = email
+        this.senha = senha
+        this.cep = cep
+        this.logradouro = logradouro
+        this.bairro = bairro
+        this.cidade = cidade
+        this.estado = estado
+    }
+}
