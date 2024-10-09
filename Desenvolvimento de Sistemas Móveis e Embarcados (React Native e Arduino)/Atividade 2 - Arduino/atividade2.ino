352e4979820778e1b679f3e6ee0d90a1a70f6cc8
Gustavo Beretta Gonçalves <gustavoberettag@gmail.com>
2024-10-09 16:53:31 -0300

    atividade2 - arduino

---
+++ Desenvolvimento de Sistemas Móveis e Embarcados (React Native e Arduino)/Atividade 2 - Arduino/atividade2.ino
@@ -0,0 +1,39 @@
+#include <Servo.h>
+
+const int trigPin = 7;
+const int echoPin = 8;
+const int potPin = A0;
+Servo servoMotor;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  servoMotor.attach(9);
+  servoMotor.write(0);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int valorPot = analogRead(potPin);
+  int distanciaLimite = map(valorPot, 0, 1023, 0, 100);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  long duracao = pulseIn(echoPin, HIGH);
+  int distancia = duracao * 0.034 / 2;
+
+  Serial.print("Distancia: ");
+  Serial.print(distancia);
+  Serial.print(" cm | Limiar: ");
+  Serial.print(distanciaLimite);
+  Serial.println(" cm");
+
+  if (distancia <= distanciaLimite) {
+    servoMotor.write(90);
+  } else {
+    servoMotor.write(0);
+  }
+
+  delay(500);
+}
